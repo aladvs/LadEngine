dd637a804b850e5389369bb6759da29691428285
Lad <flyingcaemail@gmail.com>
2023-02-05 15:45:22 -0600

    K

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+c.height = window.innerHeight;
+c.width = window.innerWidth;
+ctx.translate(c.width / 2, c.height / 2)
+function clearScreen() {
+  ctx.translate(c.width / -2, c.height / -2)
+  c.height = window.innerHeight;
+  c.width = window.innerWidth;
+  ctx.clearRect(0, 0, c.width, c.height)
+  ctx.translate(c.width / 2, c.height / 2)
+};
 function rotation() {
 
 }
