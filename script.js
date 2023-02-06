5f3d18b54c372fc04ad1cdaccaa47634cd66b38d
Lad <flyingcaemail@gmail.com>
2023-02-05 18:51:15 -0600

    drawline

--- script.js
+++ script.js
@@ -10,6 +10,14 @@ function clearScreen() {
   ctx.clearRect(0, 0, c.width, c.height)
   ctx.translate(c.width / 2, c.height / 2)
 };
+function drawLine(ctx, x1, y1, x2, y2, stroke = 'black', width = 3) {
+  ctx.beginPath();
+  ctx.moveTo(x1, -y1);
+  ctx.lineTo(x2, -y2);
+  ctx.strokeStyle = stroke;
+  ctx.lineWidth = width;
+  ctx.stroke();
+};
 //function rotation() {
 //}
 function inputhandler() {
@@ -19,6 +27,6 @@ function render() {
 
 }
 function update() {
-
+  drawLine(ctx, 0, 0, 120, 0, 'black', 6);
 }
 setInterval(update, 10);
