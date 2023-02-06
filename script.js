7bd64ae0d45db1f582903307592856d18ab7fcf5
Lad <flyingcaemail@gmail.com>
2023-02-05 19:02:11 -0600

    things

--- script.js
+++ script.js
@@ -2,21 +2,55 @@ var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 c.height = window.innerHeight;
 c.width = window.innerWidth;
-ctx.translate(c.width / 2, c.height / 2)
+halfheight = c.height / 2;
+halfwidth = c.width /2;
+ctx.translate(halfwidth, halfheight)
 function clearScreen() {
-  ctx.translate(c.width / -2, c.height / -2)
+  ctx.translate(halfwidth* -1, halfheight * -1)
   c.height = window.innerHeight;
   c.width = window.innerWidth;
   ctx.clearRect(0, 0, c.width, c.height)
-  ctx.translate(c.width / 2, c.height / 2)
+  ctx.translate(halfwidth, halfheight)
 };
 function drawLine(ctx, x1, y1, x2, y2, stroke = 'black', width = 3) {
-  ctx.beginPath();
-  ctx.moveTo(x1, -y1);
-  ctx.lineTo(x2, -y2);
-  ctx.strokeStyle = stroke;
-  ctx.lineWidth = width;
-  ctx.stroke();
+  //very inefficient but I don't want to troubleshoot canvas funkyness
+  //If past screen bounds then it sets it to max/min.
+  if (x1 > halfwidth) {
+    x1 = halfwidth;
+  }
+  if (x2 > halfwidth) {
+    x2 = halfwidth;
+  }
+  if (x1 < halfwidth * -1) {
+    x1 = halfwidth* -1;
+  }
+  if (x2 < halfwidth * -1) {
+    x2 = halfwidth* -1;
+  }
+  if (y1 > halfheight) {
+    y1 = halfheight;
+  }
+  if (y2 > halfheight) {
+    y2 = halfheight;
+  }
+  if (y1 < halfheight * -1) {
+    y1 = halfheight * -1;
+  }
+  if (y2 < halfheight * -1) {
+    y2 = halfheight * -1;
+  }
+  if((x1>halfwidth&&x2>halfwidth)||(x1<halfwidth*-1&&x2<halfwidth*-1)||(y1>halfheight&&y2>halfheight)||(y1<halfheight*-1&&y1<halfheight*-1)) {
+
+  }
+  else {
+    ctx.beginPath();
+    ctx.moveTo(x1, -y1);
+    ctx.lineTo(x2, -y2);
+    ctx.strokeStyle = stroke;
+    ctx.lineWidth = width;
+    ctx.stroke();
+    //draws line with Y-up vertices (thank god)
+  }
 };
 //function rotation() {
 //}
@@ -27,6 +61,5 @@ function render() {
 
 }
 function update() {
-  drawLine(ctx, 0, 0, 120, 0, 'black', 6);
 }
 setInterval(update, 10);
